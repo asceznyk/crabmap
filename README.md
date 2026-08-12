c72643fde4e82ba661dd12c639426a1608836dde
asceznyk <asceznyk@gmail.com>
2026-08-12 09:19:09 +0530

    another crappy commit!

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# crabstore
-This is a distribued KV store for your self-managed infra - Inspired by [minikeyvalue](https://github.com/geohot/minikeyvalue). Coming soon!
+# crabmap
+This is a distribued KV map for your self-managed infra - Inspired by [minikeyvalue](https://github.com/geohot/minikeyvalue). Coming soon!
 
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,5 @@
 use axum::{extract::{Path, Request}, routing::any, Router};
 
-#[tokio::main]
-async fn main() {
-  let mut args = std::env::args(); args.next();
-  let port = args.next().unwrap();
-  let app = Router::new().route("/{*.key}", any(dispatch));
-  let listener = tokio::net::TcpListener::bind(format!("localhost:{port}"))
-    .await
-    .unwrap();
-  println!("Server running on http://localhost:{port}");
-  axum::serve(listener, app).await.unwrap();
-}
-
 async fn dispatch(Path(key):Path<String>, request:Request) -> &'static str {
   match request.method().as_str() {
     "GET" => handle_get(&key).await,
@@ -48,4 +36,15 @@ async fn handle_rebalance(key:&str) -> &'static str {
   "REBALANCE /: I wanna be inside ya..."
 }
 
+#[tokio::main]
+async fn main() {
+  let mut args = std::env::args(); args.next();
+  let port = args.next().unwrap();
+  let app = Router::new().route("/{*.key}", any(dispatch));
+  let listener = tokio::net::TcpListener::bind(format!("localhost:{port}"))
+    .await
+    .unwrap();
+  println!("Server running on http://localhost:{port}");
+  axum::serve(listener, app).await.unwrap();
+}
 
